c0fb971d7f1b0b7e2b3fe08c0277ea477959eb51
Sophia He <SophiaHe@users.noreply.github.com>
2020-03-16 18:55:37 -0400

    Add files via upload

--- covid19_code.R
+++ covid19_code.R
@@ -1,18 +1,36 @@
 library(tidyverse)
 library(lubridate)
 library(plotly)
+library(gganimate)
 
 setwd("C:/Users/hyq92/Documents/R/Covid19")
 
 df <- read.csv("covid_19_clean_complete.csv", header = T)
 
-df2 <- df %>% mutate(Date = as.Date(paste0(as.character(Date),"20"),"%m/%d/%Y")) %>% 
+df3 <- df %>% mutate(Date = as.Date(paste0(as.character(Date),"20"),"%m/%d/%Y")) %>% 
   group_by(Date,Country.Region) %>% 
   summarize(total_confirmed = sum(Confirmed), 
             total_deaths = sum(Deaths), 
             total_recovered = sum(Recovered))
 
-a <- df2 %>% filter(Country.Region %in% c("US", "Canada")) %>% as.data.frame()
+df2 <- df %>%
+  mutate(date = as.Date(Date, "%m/%d/%y")) %>%
+  group_by(country = `Country.Region`, date) %>%
+  summarise(cases = sum(Confirmed), 
+            total_deaths= sum(Deaths),
+            total_recovered = sum(Recovered)) %>%
+  group_by(country) %>%
+  mutate(days_since_100 = as.numeric(date-min(date[cases >= 50]))) %>%
+  ungroup() %>%
+  filter(is.finite(days_since_100)) %>% 
+  group_by(country) %>%
+  mutate(new_cases = cases-cases[days_since_100 == 0],
+         new_deaths = total_deaths - total_deaths[days_since_100 == 0],
+         new_recovery = total_recovered - total_recovered[days_since_100 == 0]) %>%
+  filter(sum(cases >= 50) >= 5) %>%
+  filter(cases >= 50) %>% dplyr::select(-total_deaths, -total_recovered)
+
+a <- df2 %>% filter(country %in% c("Italy", "Korea, South", "Iran", "Spain", "Germany", "US"))
 
 # p <- ggplot(a, aes(Date, total_confirmed, color = Country.Region)) +
 #   geom_point(aes(size = total_deaths,frame = Date, ids = Country.Region))
@@ -28,9 +46,9 @@ a <- df2 %>% filter(Country.Region %in% c("US", "Canada")) %>% as.data.frame()
 #   )
 # fig
 
-p <- ggplot(a, aes(Date, total_confirmed, color=Country.Region)) +
-  geom_line() + geom_text(aes(x = max(Date)+.1, label = Country.Region, hjust=0) +
-  transition_reveal(Date) + 
+p <- ggplot(a, aes(days_since_100, cases, color=country)) +
+  geom_line() + geom_text(aes(x = max(days_since_100)+.1, label = country, hjust=0)) +
+  transition_reveal(days_since_100) + 
   # labs(title = "Date: {frame_time}") +
   view_follow(fixed_y = TRUE)+
   coord_cartesian(clip = 'off') + 
